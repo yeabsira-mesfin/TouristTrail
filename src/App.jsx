f9dcb0775e7dc47023d87ac48d32b660614fd364
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-24 06:13:58 -0400

    Sort places by distance

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState,useEffect } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -10,15 +10,20 @@ import { sortPlacesByDistance } from "./loc.js";
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces,setAvailablePlaces] = useState([])
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+         AVAILABLE_PLACES,
+         position.coords.latitude,
+         position.coords.longitude
+       );
+       setAvailablePlaces(sortedPlaces);
+     });
+  },[]);
 
-  navigator.geolocation.getCurrentPosition((position) => {
-   const sortedPlaces = sortPlacesByDistance(
-      AVAILABLE_PLACES,
-      position.coords.latitude,
-      position.coords.longitude
-    );
-  });
+ 
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -72,7 +77,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
